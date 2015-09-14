da8ea7030afbaceb12a951e640562e329624a962
Wolfgang Baird <aguywithlonghair@yahoo.com>
2015-09-14 12:53:43 -0700

    _fullscreenER
    - Fix for crash on 10.10

--- fullscreenER/_fullscreenER/Load.m
+++ fullscreenER/_fullscreenER/Load.m
@@ -2,67 +2,66 @@
 //  Load.m
 //  _fulllscreenER
 //
-//  Created by Wolfgang Baird on 9/10/15.
-//  Copyright © 2015 Wolfgang Baird. All rights reserved.
+//  Created by Wolfgang on 9/13/15.
+//  Copyright (c) 2015 Wolfgang. All rights reserved.
 //
 
-#import "load.h"
+#import "Load.h"
 #import "ZKSwizzle.h"
 @import AppKit;
 
 BOOL _isMaximized = NO;
 BOOL _willMaximize = NO;
+NSInteger osx_ver;
 struct CGRect _cachedFrame;
 
 @implementation Load
 
 + (void)load {
-    NSLog(@"OS X 10.11, _fullscreenER loaded...");
-//    _cachedFrame = CGRectMake(0, 0, 0, 0);
+    osx_ver = [[NSProcessInfo processInfo] operatingSystemVersion].minorVersion;
+    NSLog(@"OS X 10.%ld, _fullscreenER loaded...", (long)osx_ver);
+    
     NSApplication *application = [NSApplication sharedApplication];
     if (application.windows)
     {
         for (NSWindow *win in application.windows)
         {
             win.styleMask = win.styleMask | NSResizableWindowMask;
-//            [win setTitlebarAppearsTransparent:true];
+            //            win.styleMask = NSResizableWindowMask | NSBorderlessWindowMask;
+            //            [win setTitlebarAppearsTransparent:true];
             
-//            Class vibrantClass=NSClassFromString(@"NSVisualEffectView");
-//            if (vibrantClass)
-//            {
-//                NSVisualEffectView *vibrant=[[vibrantClass alloc] initWithFrame:[[win contentView] bounds]];
-//                [vibrant setAutoresizingMask:NSViewWidthSizable|NSViewHeightSizable];
-//                [vibrant setBlendingMode:NSVisualEffectBlendingModeBehindWindow];
-//                [vibrant setIdentifier:@"w0blurView"];
-//                if (![win.contentView.subviews containsObject:vibrant])
-//                    [[win contentView] addSubview:vibrant positioned:NSWindowBelow relativeTo:nil];
-//            }
+            /*
+             Class vibrantClass=NSClassFromString(@"NSVisualEffectView");
+             if (vibrantClass)
+             {
+             NSVisualEffectView *vibrant=[[vibrantClass alloc] initWithFrame:[[win contentView] bounds]];
+             [vibrant setAutoresizingMask:NSViewWidthSizable|NSViewHeightSizable];
+             [vibrant setBlendingMode:NSVisualEffectBlendingModeBehindWindow];
+             [vibrant setIdentifier:@"w0blurView"];
+             if (![win.contentView.subviews containsObject:vibrant])
+             [[win contentView] addSubview:vibrant positioned:NSWindowBelow relativeTo:nil];
+             }
+             */
             
             /*
              NSClosableWindowMask |
-             NSTitledWindowMask | 
-             NSMiniaturizableWindowMask | 
-             NSTexturedBackgroundWindowMask | 
-             NSResizableWindowMask | 
+             NSTitledWindowMask |
+             NSMiniaturizableWindowMask |
+             NSTexturedBackgroundWindowMask |
+             NSResizableWindowMask |
              NSFullSizeContentViewWindowMask;
-             
-             NSWindowFullScreenButton
-             NSWindow *this = (NSWindow*)self;
-             NSButton *windowButton = [this standardWindowButton:NSWindowZoomButton];
-             NSButton *t = [[NSWindow alloc] standardWindowButton:NSWindowZoomButton];
-             windowButton = t;
-             
              */
-           
         }
     }
 }
 
 @end
 
+
 @interface NSWindow (Maximizer)
 - (void)setFrame:(struct CGRect)arg1 display:(BOOL)arg2 animate:(BOOL)arg3;
 - (struct CGRect)_tileFrameForFullScreen;
+- (struct CGRect)_frameForFullScreenMode;
 - (BOOL)_inFullScreen;
 @end
 
@@ -108,15 +107,21 @@ ZKSwizzleInterface(_Maximize_NSWindow, NSWindow, NSResponder);
     NSWindow *this = (NSWindow*)self;
     
     if ([this _inFullScreen]) {
-
+        
         [this toggleFullScreen:this];
         
     } else {
-    
+        
         if (!_isMaximized) {
             _isMaximized = YES;
             _cachedFrame = this.frame;
-            [this setFrame:[this _tileFrameForFullScreen] display:true animate:true];
+            if (osx_ver < 11) {
+                // 10.10
+                [this setFrame:[this _frameForFullScreenMode] display:true animate:true];
+            } else {
+                // 10.11+
+                [this setFrame:[this _tileFrameForFullScreen] display:true animate:true];
+            }
         } else {
             _isMaximized = NO;
             [this setFrame:_cachedFrame display:true animate:true];
@@ -131,11 +136,11 @@ ZKSwizzleInterface(_Maximize_NSWindow, NSWindow, NSResponder);
     if (([[NSApp currentEvent] modifierFlags] & NSAlternateKeyMask) == NSAlternateKeyMask) {
         [[this standardWindowButton:NSWindowZoomButton] setAction:@selector(_W0fullScreen)];
         [[this standardWindowButton:NSWindowZoomButton] setAlphaValue:.5 ];
-//        NSLog(@"Fullscreen");
+        //        NSLog(@"Fullscreen");
     } else {
         [[this standardWindowButton:NSWindowZoomButton] setAction:@selector(_W0fillScreen)];
         [[this standardWindowButton:NSWindowZoomButton] setAlphaValue:1 ];
-//        NSLog(@"Plus");
+        //        NSLog(@"Plus");
     }
     
     return NO;
